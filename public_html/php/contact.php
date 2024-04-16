cd57e45b7f192e9fabbb18d9e03b5215376b5f2a
Ignacio Orco <ignacioorco@gmail.com>
2024-04-16 15:24:22 -0300

    primer commit

--- public_html/php/contact.php
+++ public_html/php/contact.php
@@ -1,44 +1,54 @@
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Sanitize user input
-    $name = htmlspecialchars($_POST['name']);
-    $email = htmlspecialchars($_POST['email']);
-    $comments = htmlspecialchars($_POST['comments']);
+/**
+ * @version 1.0
+ */
 
-    // Set the recipient email address
-    $to = "info@mitogafas.com.ar";
+require("class.phpmailer.php");
+require("class.smtp.php");
 
-    // Set the email subject
-    $subject = "New mensaje del formulario de contacto";
-
- // Set the email content
-    $message = "
-     <html>
-     <head>
-    <title>New mensaje del formulario de contacto</title>
-     </head>
-     <body>
-     <p>Reciviste un mensaje del formulario de contacto.</p>
-    <p>Nombre: $name</p>
-     <p>Email: $email</p>
-     <p>Mensaje: $comments</p>
-     </body>
-    </html>
-    ";
-    // $txt ="Nombre = ". $name . "\r\n  Email = "
-    // . $email . "\r\n Mensaje =" . $message;
-    // Set the email headers
-    $headers = "MIME-Version: 1.0". "\r\n";
-    $headers.= "Content-type:text/html;charset=UTF-8". "\r\n";
-   $headers.= "From: $email". "\r\n";
-
-    // Send the email
-    if (mail($to, $subject, $txt, $headers)) {
-        // If the email is sent successfully, display a success message
-        echo "<div id='simple-msg'>El mensaje a sido  enviado correctamente!</div>";
-    } else {
-        // If there's an error sending the email, display an error message
-        echo "<div id='simple-msg'>perdon, tu mensaje no a sido enviado correctamente.</div>";
-    }
+// Valores enviados desde el formulario
+if ( !isset($_POST["nombre"]) || !isset($_POST["email"]) || !isset($_POST["mensaje"]) ) {
+    die ("Es necesario completar todos los datos del formulario");
 }
-?>
+$nombre = $_POST["nombre"];
+$email = $_POST["email"];
+$mensaje = $_POST["mensaje"];
+
+// Datos de la cuenta de correo utilizada para enviar vía SMTP
+$smtpHost = "c1901928.ferozo.com";  // Dominio alternativo brindado en el email de alta 
+$smtpUsuario = "info@mitogafas.com.ar";  // Mi cuenta de correo
+$smtpClave = "@Lascamelias33";  // Mi contraseña
+
+// Email donde se enviaran los datos cargados en el formulario de contacto
+$emailDestino = "info@mitogafas.com.ar";
+
+$mail = new PHPMailer();
+$mail->IsSMTP();
+$mail->SMTPAuth = true;
+$mail->Port = 465; 
+$mail->SMTPSecure = 'ssl';
+$mail->IsHTML(true); 
+$mail->CharSet = "utf-8";
+
+
+// VALORES A MODIFICAR //
+$mail->Host = $smtpHost; 
+$mail->Username = $smtpUsuario; 
+$mail->Password = $smtpClave;
+
+$mail->From = $email; // Email desde donde envío el correo.
+$mail->FromName = $nombre;
+$mail->AddAddress($emailDestino); // Esta es la dirección a donde enviamos los datos del formulario
+
+$mail->Subject = "DonWeb - Ejemplo de formulario de contacto"; // Este es el titulo del email.
+$mensajeHtml = nl2br($mensaje);
+$mail->Body = "{$mensajeHtml} <br /><br />Formulario de ejemplo. By DonWeb<br />"; // Texto del email en formato HTML
+$mail->AltBody = "{$mensaje} \n\n Formulario de ejemplo By DonWeb"; // Texto sin formato HTML
+// FIN - VALORES A MODIFICAR //
+
+$estadoEnvio = $mail->Send(); 
+if($estadoEnvio){
+    echo "El correo fue enviado correctamente.";
+} else {
+    echo "Ocurrió un error inesperado.";
+}
